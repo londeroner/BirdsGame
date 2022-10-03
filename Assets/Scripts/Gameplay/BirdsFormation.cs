37d89cc9e48ce4884d480fdd0e1ee2e80ef130be
londeroner <Filin_s1999@mail.ru>
2022-10-03 13:41:05 +0300

    Добавлены HP-бары над формациями;

--- Assets/Scripts/Gameplay/BirdsFormation.cs
+++ Assets/Scripts/Gameplay/BirdsFormation.cs
@@ -2,30 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BirdsFormation : MonoBehaviour
 {
     public int Health = 10;
+    private int maxHealth;
 
     [NonSerialized]
     public bool _fight = false;
 
-    private void Update()
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
+    void Start()
     {
-        if (Health <= 0)
-            Destroy(gameObject);
+        maxHealth = Health;    
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == Consts.FormationTag && !_fight)
         {
-            Health--;
-            _fight = true;
+            TakeDamage(1);
 
-            var otherFormation = other.gameObject.GetComponent<BirdsFormation>();
-            otherFormation.Health--;
-            otherFormation._fight = true;
+            other.gameObject.GetComponent<BirdsFormation>().TakeDamage(1);
         }
     }
 
@@ -33,5 +34,15 @@ public class BirdsFormation : MonoBehaviour
     {
         if (other.tag == Consts.FormationTag)
             _fight = false;
+    }
+
+    private void TakeDamage(int amount)
+    {
+        Health -= amount;
+        _fight = true;
+        healthBar.fillAmount = ((float)Health * 100 / (float)maxHealth) / 100;
+
+        if (Health <= 0)
+            Destroy(gameObject);
     }
 }
